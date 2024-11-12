25cdada33ad786057d15cc7232f91557884df815
Vojtech David <vojtech.david@chyron.com>
2024-11-13 00:11:28 +0100

    wip

--- main.cpp
+++ main.cpp
@@ -4,14 +4,12 @@
 
 TEST_CASE("simple")
 {
-
   auto path = Word2Word::word2word("words.txt", "cat", "dog");
   REQUIRE(path.size() == 4);
   REQUIRE(path[0] == "cat");
   REQUIRE(path[1] == "cot");
   REQUIRE(path[2] == "cog");
   REQUIRE(path[3] == "dog");
-
 }
 
 TEST_CASE("start equals end")
@@ -25,7 +23,14 @@ TEST_CASE("start equals end")
 TEST_CASE("sequence does not exists")
 {
 
-  auto path = Word2Word::word2word("words.txt", "zygose", "zygoid");
-  REQUIRE(path[0] == "zygoid");
+  auto path = Word2Word::word2word("words.txt", "xylographical", "xenodiagnoses");
   REQUIRE(path.size() == 1);
+  REQUIRE(path[0] == "xenodiagnoses");
+}
+
+TEST_CASE("sequence does not exists switched")
+{
+  auto path = Word2Word::word2word("words.txt", "xenodiagnoses", "xylographical");
+  REQUIRE(path.size() == 1);
+  REQUIRE(path[0] == "xylographical");
 }
